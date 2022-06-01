1dd76f3e65ad167c0a4e7690cb84184f3560db06
Dominik.Kotecki <dominikus1910@gmail.com>
2022-06-01 16:28:47 +0000

    add simple login endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,43 @@ import express_pino from "express-pino-logger"
 import body_parser from "body-parser"
 import passport from "passport"
 import fake from "./infrastructure/auth/fake"
+import jwt from 'jsonwebtoken';
+
 const app = express()
 passport.use(fake())
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(express_pino({logger: pino({level: "debug"})}))
 
-app.get('/', async function (req, res) {
-  res.send('hello world2')
-})
+app.post(
+  '/login',
+  async (req, res, next) => {
+    passport.authenticate(
+      'login',
+      async (err, user, info) => {
+        try {
+          if (err || !user) {
+            const error = new Error('An error occurred.');
+
+            return next(error);
+          }
+
+          req.login(
+            user,
+            { session: false },
+            async (error) => {
+              if (error) return next(error);
+              const body = { _id: user._id, email: user.email };
+              const token = jwt.sign({ user: body }, 'TOP_SECRET');
+
+              return res.json({ token });
+            }
+          );
+        } catch (error) {
+          return next(error);
+        }
+      }
+    )(req, res, next);
+  }
+);
 
 app.listen(3000)
